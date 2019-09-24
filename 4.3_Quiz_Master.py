43f969fd2bee30fc1f98c3525af6ea94440edcb3
catherineuhs <cliffordcat@urbandaleschools.com>
2019-09-24 14:23:20 -0500

    program

--- 4.3_Quiz_Master.py
+++ 4.3_Quiz_Master.py
@@ -4,3 +4,12 @@ QUIZ MASTER PROJECT
 The criteria for the project are on the website. Make sure you test this quiz with 
 two of your student colleagues before you run it by your instructor.
 '''
+
+#
+
+
+
+
+number = input(int("Choose one of the following numbers: 1, 2, 3, 4"))
+if number == 1:
+    print("nice.")
