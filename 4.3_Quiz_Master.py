b970753a9e394a6b440535ae5a55c4c56e322349
catherineuhs <cliffordcat@urbandaleschools.com>
2019-09-30 14:23:48 -0500

    program

--- 4.3_Quiz_Master.py
+++ 4.3_Quiz_Master.py
@@ -10,11 +10,17 @@ two of your student colleagues before you run it by your instructor.
 
 
 print("welcome to the quiz!")
-one = int(input("Choose one of the following numbers: 1, 2, 3, 4"))
+one = input("(T/F)
 if one == 1:
     print("Fun Fact: New York was briefly named 'New Orange'")
 else:
     print("Fun Fact: Competitive Art used to be in the Olympics.")
-two = int(input("You've completed the first quest"))
+
+three = input("How many people are in the World? \n A. 10.3 million \n B. 7.53 billion \n C. 6.7.54 billion \n D. 7.66 trillion \n Answer: ")
+if three == "D" or "d":
+    print("correct!")
+
+
+
 
 
