dbeebe0c16bf81307f54e38f2bfbd3129c8fc6ff
catherineuhs <cliffordcat@urbandaleschools.com>
2019-10-01 14:22:36 -0500

    program

--- 4.3_Quiz_Master.py
+++ 4.3_Quiz_Master.py
@@ -10,15 +10,23 @@ two of your student colleagues before you run it by your instructor.
 
 
 print("welcome to the quiz!")
-one = input("(T/F)
-if one == 1:
-    print("Fun Fact: New York was briefly named 'New Orange'")
+point = 0
+one = input("(T/F) The cigarette lighter was invented before the match. \n Answer:")
+if one == "true" or "T" or "t":
+    print("correct! Score:", point)
+    point = +1
 else:
-    print("Fun Fact: Competitive Art used to be in the Olympics.")
+    print("wrong.")
 
-three = input("How many people are in the World? \n A. 10.3 million \n B. 7.53 billion \n C. 6.7.54 billion \n D. 7.66 trillion \n Answer: ")
-if three == "D" or "d":
-    print("correct!")
+three = input("\n \nHow many people are in the World? \n A. 10.3 million \n B. 7.53 billion \n C. 6.7 billion \n D. 7.66 trillion \n Answer: ")
+if three == "B" or "b" or "7.53":
+    print("correct! Score:", point)
+point = +1
+else:
+print("wrong.")
+e = input("")
+
+
 
 
 
