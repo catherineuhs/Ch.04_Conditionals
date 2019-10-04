b03bfed64979ad2b5f5b08e43848b286051ee884
catherineuhs <cliffordcat@urbandaleschools.com>
2019-10-04 14:23:23 -0500

    quiz

--- 4.3_Quiz_Master.py
+++ 4.3_Quiz_Master.py
@@ -10,15 +10,20 @@ two of your student colleagues before you run it by your instructor.
 
 
 score = 0
-print("Welcome to the quiz!")
+print("Welcome to the quiz! These questions will test your knowledge on strange facts!")
 print("(T/F) The cigarette lighter was invented before the match.")
 
 
 
 user_input = input("Answer:")
 score = 0
-if user_input == "T":
+if user_input.lower() == "t":
    score = score + 1
+   print("Correct!")
+   print("Score: ", score)
+elif user_input.lower() == "true":
+   score = score + 1
+   print("Correct!")
    print("Score: ", score)
 
 else:
@@ -32,7 +37,7 @@ print("A. 10.3 million \n B. 7.53 billion \n C. 6.7 billion \n D. 7.66 trillion 
 
 user_input2 = input("Answer:")
 
-if user_input2 == "B" and "b":
+if user_input2.lower() == "b":
     score = score + 1
     print("correct!")
     print("Score: ", score)
@@ -59,10 +64,19 @@ score = 0
 print("\n \n Who is one of the founders of the Apple company?")
 user_input4 = input("Answer:")
 
-if user_input4 == "Steve Jobs" and "Ronald Wayne" and "Steve Wozniac":
+if user_input4.lower() == "steve jobs":
     score = score +1
     print("Correct!")
     print("Score:", score)
+elif user_input4.lower() == "ronald wayne":
+    score = score + 1
+    print("Correct!")
+    print("Score:", score)
+elif user_input4.lower() == "steve wozniac":
+    score = score + 1
+    print("Correct!")
+    print("Score:", score)
+
 else:
     print("wrong")
     print("Score:", score)
@@ -73,7 +87,7 @@ else:
 print("\n \n(T/F)Its physically impossible to lick your elbow")
 user_input5 = input("Answer:")
 
-if user_input5 == "T" or "t" or "true":
+if user_input5.lower() == "T" or "true":
     print("Correct")
     score = score + 1
     print("Score:", score)
@@ -82,7 +96,7 @@ else:
     print("Score:", score)
 
 print("When did NASA officially land on the moon? \n A. 1970 \n B. 1999 \n C. 1962 \n D. 1969")
-user_input6 = int(input("Answer:"))
+user_input6 = input("Answer:")
 
 if user_input6 == "D" or "d" or "1969":
     print("Correct!")
