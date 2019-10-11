9d37ea5bdfe9c1f27a685b1921914e19bf40fa6f
catherineuhs <cliffordcat@urbandaleschools.com>
2019-10-11 14:09:28 -0500

    quiz

--- 4.3_Quiz_Master.py
+++ 4.3_Quiz_Master.py
@@ -10,8 +10,8 @@ two of your student colleagues before you run it by your instructor.
 
 
 score = 0
-print("Welcome to the quiz! These questions will test your knowledge on strange facts!")
-print("(T/F) The cigarette lighter was invented before the match.")
+print(" \n \n Welcome to the quiz! These questions will test your knowledge on strange facts!")
+print("\n \n \n 1. (T/F) The cigarette lighter was invented before the match.")
 
 
 
@@ -32,7 +32,7 @@ else:
 
 
 
-print(" \n \n How many people are in the world?")
+print(" \n \n 2. How many people are in the world?")
 print("A. 10.3 million \n B. 7.53 billion \n C. 6.7 billion \n D. 7.66 trillion \n ")
 
 user_input2 = input("Answer:")
@@ -46,7 +46,7 @@ else:
     print("wrong")
     print("Score:", score)
 
-print("\n \nWhats 3 + 4?")
+print("\n \n 3. Whats 3 + 4?")
 user_input3 = int(input("Answer:"))
 
 if user_input3 == 7:
@@ -59,13 +59,12 @@ else:
 
 
 
-score = 0
 
-print("\n \n Who is one of the founders of the Apple company?")
+print("\n \n 4. Who is one of the founders of the Apple company?")
 user_input4 = input("Answer:")
 
 if user_input4.lower() == "steve jobs":
-    score = score +1
+    score = score + 1
     print("Correct!")
     print("Score:", score)
 elif user_input4.lower() == "ronald wayne":
@@ -84,21 +83,26 @@ else:
 
 
 
-print("\n \n(T/F)Its physically impossible to lick your elbow")
+print("\n \n 5. (T/F)Its physically impossible to lick your elbow")
 user_input5 = input("Answer:")
 
-if user_input5.lower() == "T" or "true":
+if user_input5.lower() == "t" or "true":
     print("Correct")
     score = score + 1
     print("Score:", score)
+
+elif user_input5.lower() == "true":
+    print("Correct")
+    score = score + 1
+    print("Score:", score)
+
 else:
     print("wrong")
     print("Score:", score)
 
-print("When did NASA officially land on the moon? \n A. 1970 \n B. 1999 \n C. 1962 \n D. 1969")
+print(" \n \n 6. When did NASA officially land on the moon? \n A. 1970 \n B. 1999 \n C. 1962 \n D. 1969")
 user_input6 = input("Answer:")
-
-if user_input6 == "D" or "d" or "1969":
+if user_input6.lower() == "d":
     print("Correct!")
     score = score + 1
     print("Score:", score)
@@ -106,8 +110,9 @@ if user_input6 == "D" or "d" or "1969":
 else:
     print("wrong")
     print("Score:", score)
-score = 4
-print("Whats 30 plus 6?")
+
+
+print(" \n \n 7. Whats 30 plus 6?")
 user_input7 = int(input("Answer:"))
 
 if user_input7 == 36:
@@ -119,7 +124,7 @@ else:
     print("Score:", score)
 
 percentage = score
-print("You've completed the test! Rerun code if you want to retake for a better score.")
+print(" \n \n \nYou've completed the test! Rerun code if you want to retake for a better score.")
 print("Test score:", percentage, "/ 7")
 
 
